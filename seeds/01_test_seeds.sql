e27eca48e83adeed908b1d7d9a9f588618490adc
FemmeSTEMGem <femmestemgem@gmail.com>
2022-02-12 15:59:22 -0500

    Add seeds folder and 01_ test_seeds file. Test with SELECTs.

---
+++ seeds/01_test_seeds.sql
@@ -0,0 +1,11 @@
+INSERT INTO users (name, password, email) VALUES ('Bridgette Granville', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.', 'bridgette@gmail.com' );
+INSERT INTO users (name, password, email) VALUES('Riley Mathewson', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.', 'riley@gmail.com');
+
+INSERT INTO list_items (entry, category, user_id) VALUES ('Over the Top', 'to_watch', 1);
+INSERT INTO list_items (entry, category, user_id) VALUES ('Piano Piano', 'to_eat', 1);
+INSERT INTO list_items (entry, category, user_id) VALUES ('The Hogfather', 'to_read', 1);
+INSERT INTO list_items (entry, category, user_id) VALUES ('Sennheiser HD 800 S', 'to_buy', 1);
+INSERT INTO list_items (entry, category, user_id) VALUES ('Dune', 'to_watch', 2);
+INSERT INTO list_items (entry, category, user_id) VALUES ('Biffs', 'to_eat', 2);
+INSERT INTO list_items (entry, category, user_id) VALUES ('The Martian', 'to_read', 2);
+INSERT INTO list_items (entry, category, user_id) VALUES ('SteelSeries Apex Pro', 'to_buy', 2);
